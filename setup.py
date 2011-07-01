6221f90273fa5f23c08416b1eede4a2aaa6526ce
Michael Leonhard <mike@restbackup.com>
2011-07-01 09:28:39 +0000

    Version 1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='restbackup-python',
-    version='1.4',
+    version='1.5',
     packages = find_packages(),
     description = 'RestBackup(tm) Client Library and Command Line Interface',
     url = 'https://github.com/mleonhard/restbackup-python',
